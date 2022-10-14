0075a3ca8f8607cb995e571d3aecba826656db1e
RubaCodes <rubacodes@gmail.com>
2022-10-14 14:20:59 +0200

    chiamata api generata correttamente

---
+++ Controllers/Api/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_static;
+using la_mia_pizzeria_static.Contexts;
+using la_mia_pizzeria_static.Models;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        PizzaContext _ctx;
+        public HomeController()
+        {
+            _ctx = new PizzaContext();
+        }
+
+
+        [HttpGet]
+        public IActionResult Get() {
+            List<Pizza> menu = _ctx.Pizzas.ToList();
+            return Ok(menu);
+        }
+    }
+}
