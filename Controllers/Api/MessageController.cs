1fbc6371110eb4e3e4a1b273c144ec2c6f85a2c2
RubaCodes <rubacodes@gmail.com>
2022-10-18 05:59:35 +0200

    verificato inserimento messaggio in tabella

---
+++ Controllers/Api/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using la_mia_pizzeria_static.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_static.Models;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        PizzaContext _ctx;
+        public MessageController() {
+            _ctx = new PizzaContext();
+        }
+        [HttpPost]
+        public IActionResult Send([FromBody] Message msg) {
+            if (!ModelState.IsValid) {
+                return BadRequest();
+            }
+            _ctx.Messages.Add(msg);
+            _ctx.SaveChanges();
+            return Ok("Messaggio inviato");
+        }
+    }
+}
