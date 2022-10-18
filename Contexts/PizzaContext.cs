5ac249b41177b6455dadf1e404c47c58deae53f4
RubaCodes <rubacodes@gmail.com>
2022-10-18 04:01:55 +0200

    aggiunto mdello / migrazione messages

--- Contexts/PizzaContext.cs
+++ Contexts/PizzaContext.cs
@@ -12,6 +12,7 @@ namespace la_mia_pizzeria_static.Contexts
         public DbSet<Pizza> Pizzas { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilding)
         {
---
+++ Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "La mail e' obbligatoria")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Il nome deve essere presente.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Il titolo del messaggio e' obbligatorio.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Il corpo del messaggio e' obblicatorio.")]
+        public string Text { get; set; }
+
+    }
+}
